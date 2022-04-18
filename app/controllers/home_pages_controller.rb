23b24f450fa75cd434a80187605bdd11132b54e4
Buyaki <rittahbuyaki@gmail.com>
2022-04-18 19:49:47 +0300

    Add action create in the home pages controller

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -5,6 +5,18 @@ class HomePagesController < ApplicationController
     @home_page = HomePage.new
   end
 
-  def create 
+  def create
+    @new_category = current_user.home_pages.new(category_params)
+    if @new_category.save!
+      redirect_to home_pages_path, flash: { alert: 'Your category is saved' }
+    else
+      redirect_to new_home_page_path, flash: { alert: 'Could not save your category' }
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:home_page).permit(:name, :icon)
   end
 end
