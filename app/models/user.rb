2bf9952b1fc6db1a17ab1d26a0b593ec963e738e
Buyaki <rittahbuyaki@gmail.com>
2022-04-18 14:20:32 +0300

    Add validation in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true, length: { in: 1..100 }
 end
