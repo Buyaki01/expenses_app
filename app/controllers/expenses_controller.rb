f7f7fe50718e95c05d4f9086c922f45d972a02c1
Buyaki <rittahbuyaki@gmail.com>
2022-04-19 16:47:53 +0300

    Add create action in the expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,7 +2,23 @@ class ExpensesController < ApplicationController
   def index
     @expenses = Expense.all
   end
+
   def new
     @expense = Expense.new
+  end
+
+  def create
+    @new_expense = current_user.expenses.new(expense_params)
+    if @new_expense.save!
+      redirect_to home_page_expenses_path, flash: { alert: 'Your category is saved' }
+    else
+      redirect_to new_home_page_expense_path, flash: { alert: 'Could not save your category' }
+    end
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:name, :amount)
   end
 end
