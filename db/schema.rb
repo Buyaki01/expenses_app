c0b4449453bf353da04131fe9aaa85c32dd4ecbf
Buyaki <rittahbuyaki@gmail.com>
2022-04-19 15:30:28 +0300

    Add migration for expenses table referencing users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_19_093539) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_19_122557) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_19_093539) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_expenses_on_author_id"
+  end
+
   create_table "home_pages", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -65,5 +74,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_19_093539) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "expenses", "users", column: "author_id"
   add_foreign_key "home_pages", "users"
 end
