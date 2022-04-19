4474aba2d1ef892bf9f5e0d39bdbc82c1127e545
Buyaki <rittahbuyaki@gmail.com>
2022-04-19 13:34:22 +0300

    Add git Active storage to allow user to upload image

--- app/models/home_page.rb
+++ app/models/home_page.rb
@@ -1,3 +1,4 @@
 class HomePage < ApplicationRecord
   belongs_to :user
+  has_one_attached :icon
 end
--- db/migrate/20220418161345_create_home_pages.rb
+++ db/migrate/20220418161345_create_home_pages.rb
@@ -2,7 +2,6 @@ class CreateHomePages < ActiveRecord::Migration[7.0]
   def change
     create_table :home_pages do |t|
       t.string :name
-      t.string :icon
 
       t.timestamps
     end
