5acd9dc2e82f7374b665509edce4240e87c129ef
Buyaki <rittahbuyaki@gmail.com>
2022-04-20 13:31:27 +0300

    Add changes to the expenses part in the index view file for expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -17,16 +17,6 @@ class ExpensesController < ApplicationController
     end
   end
 
-  # def create
-  #   @category = HomePage.find(params[:home_page_id])
-  #   @new_expense = @category.expenses.create(expense_params)
-  #   if @new_expense.save
-  #     redirect_to home_page_expenses_path(params[:home_page_id]), flash: { alert: 'Your expense is saved' }
-  #   else
-  #     redirect_to new_home_page_expense_path, flash: { alert: 'Could not save your expense' }
-  #   end
-  # end
-
   private
 
   def expense_params
