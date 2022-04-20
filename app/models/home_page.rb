8512971ffb84e81c46b21bdff5990793d20c336c
Buyaki <rittahbuyaki@gmail.com>
2022-04-20 22:51:16 +0300

    Add validation for the expense table

--- app/models/home_page.rb
+++ app/models/home_page.rb
@@ -2,7 +2,7 @@ class HomePage < ApplicationRecord
   belongs_to :user
   has_many :expenses_categories, dependent: :destroy
   has_many :expenses, through: :expenses_categories
-  
+
   has_one_attached :icon
 
   validates :name, presence: true, length: { in: 1..20 }
