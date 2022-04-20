7dec2173f207d02146f4c747806502b1f02db062
Buyaki <rittahbuyaki@gmail.com>
2022-04-20 22:48:02 +0300

    Add correct model associations for the join table and the concerned tables

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
+
+  has_many :expenses_categories, dependent: :destroy
+  has_many :home_pages, through: :expenses_categories
 end
