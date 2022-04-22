bd032b931b77f3f6d55b3dac8f32d340ccceb79f
Buyaki <rittahbuyaki@gmail.com>
2022-04-22 17:36:56 +0300

    Add model test for expense model

--- spec/models/home_page_spec.rb
+++ spec/models/home_page_spec.rb
@@ -2,17 +2,12 @@ require 'rails_helper'
 
 RSpec.describe HomePage, type: :model do
   user = User.create(name: 'Ritta', email: 'ritta@example.com', password: '123456')
-  subject { HomePage.new(user_id: user, name: 'Food', icon: 'app/assets/images/makeup.jpg') }
+  subject { HomePage.new(user_id: user, name: 'Food') }
 
   before { subject.save }
 
   it 'check the name is not blank' do
     subject.name = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'check the icon is not blank' do
-    subject.icon = nil
     expect(subject).to_not be_valid
   end
 end
